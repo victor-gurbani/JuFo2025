ab25b0b1cccb4c53b8bbb29caa87ba8847597088
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2025-02-22 17:43:27 +0100

    added support for heic files

--- Backend/school-access-control-backend/utils/imageProcessor.js
+++ Backend/school-access-control-backend/utils/imageProcessor.js
@@ -1,16 +1,30 @@
 const sharp = require('sharp');
+const heicConvert = require('heic-convert');
 
 async function processImage(base64Image) {
   try {
     // Remove data:image/xyz;base64, prefix if present
     const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
     const imageBuffer = Buffer.from(base64Data, 'base64');
+    let processedImageBuffer;
+
+    // Check if the image is HEIC
+    if (base64Image.startsWith('data:image/heic;base64,')) {
+      const heicBuffer = await heicConvert({
+      buffer: imageBuffer,
+      format: 'JPEG', // or 'PNG'
+      quality: 1 // between 0 and 1
+      });
+      processedImageBuffer = heicBuffer;
+    } else {
+      processedImageBuffer = imageBuffer;
+    }
 
     // Process image with sharp
-    const processedImageBuffer = await sharp(imageBuffer)
+    processedImageBuffer = await sharp(processedImageBuffer)
       .resize(1024, 1024, {
-        fit: 'contain',
-        background: { r: 255, g: 255, b: 255, alpha: 1 }
+      fit: 'cover',
+      background: { r: 255, g: 255, b: 255, alpha: 1 }
       })
       .png()
       .toBuffer();
