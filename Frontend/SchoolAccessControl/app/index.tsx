1de36f7234a7f36cb9593777ed5b1233dc02198d
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2025-02-24 16:21:00 +0100

    minimal working example of expo router

--- Frontend/SchoolAccessControl/app/index.tsx
+++ Frontend/SchoolAccessControl/app/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/LoginScreen";
-import AdminPanel from "./screens/AdminPanel";
-import TeacherPanel from "./screens/TeacherPanel";
-import GuardPanel from "./screens/GuardPanel";
+import LoginScreen from "../screens/LoginScreen";
+import AdminPanel from "../screens/AdminPanel";
+import TeacherPanel from "../screens/TeacherPanel";
+import GuardPanel from "../screens/GuardPanel";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+    // <NavigationContainer>
+      <Stack.Navigator /*initialRouteName="Login"*/>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="AdminPanel" component={AdminPanel} />
         <Stack.Screen name="TeacherPanel" component={TeacherPanel} />
         <Stack.Screen name="GuardPanel" component={GuardPanel} />
       </Stack.Navigator>
-    </NavigationContainer>
+    // </NavigationContainer>
   );
 }
